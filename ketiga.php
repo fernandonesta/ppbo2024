d64f01701e92bebfc118a852f6be35421069dfee
fernandonesta <fernestadianto@student.untan.ac.id>
2024-09-04 23:20:23 +0800

    Praktikum 4B - Membuat Atribut

--- ketiga.php
+++ ketiga.php
@@ -3,15 +3,16 @@
 class Lingkaran
 {
     const PHI = 3.14;
+    public $jari_jari;
 
 
-    public function luas($jari_jari) : float {
-        return self::PHI*$jari_jari*$jari_jari;
+    public function luas() : float {
+        return self::PHI*$this->jari_jari*$this->jari_jari;
     }
 
 
-    public function keliling($jari_jari) : float {
-        return 2*self::PHI*$jari_jari;
+    public function keliling() : float {
+        return 2*self::PHI*$this->jari_jari;
     }
 }
 
@@ -42,6 +43,19 @@ class Kerucut
     }
 }
 
+
+$lingkaran = new Lingkaran();
+$lingkaran->jari_jari = 7;
+$luas_lingkaran = $lingkaran->luas();
+$keliling_lingkaran = $lingkaran->keliling();
+$bola = new Bola();
+$volume_bola = $bola->volume(4, 7);
+$tabung = new Tabung();
+$volume_tabung = $tabung->volume(2, 6);
 $nasi_tumpeng = new Kerucut();
 $volume_nasi_tumpeng = $nasi_tumpeng->volume(4, 10);
-echo "volume kerucut adalah {$volume_nasi_tumpeng}";
+echo "Luas lingkaran adalah: {$luas_lingkaran}\n";
+echo "Keliling lingkaran adalah: {$keliling_lingkaran}\n";
+echo "Volume bola adalah : {$volume_bola}\n";
+echo "Volume tabung adalah : {$volume_tabung}\n";
+echo "volume kerucut adalah {$volume_nasi_tumpeng}\n";
