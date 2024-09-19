cca20f30db138e0d3a408a507926154d122bbcd1
fernandonesta <fernestadianto@student.untan.ac.id>
2024-09-19 19:20:32 +0800

    Praktikum 6E - Menggunakan Pewarisan

---
+++ App/Admin/Pegawai.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Admin;
+
+class Pegawai {
+    public $nip;
+    public $nama;
+    protected $no_hp;
+    public $alamat;
+
+    // Konstruktor
+    public function __construct($nip, $nama, $no_hp, $alamat) {
+        $this->nip = $nip;
+        $this->nama = $nama;
+        $this->no_hp = $no_hp;
+        $this->alamat = $alamat;
+    }
+
+    // Metode untuk cek pegawai masuk
+    public function cekIn() {
+        return true; // Misalnya, pegawai berhasil cek in
+    }
+
+    // Metode untuk cek pegawai keluar
+    public function cekOut() {
+        return true; // Misalnya, pegawai berhasil cek out
+    }
+
+    // Getter untuk nomor HP
+    public function getNoHp() {
+        return $this->no_hp;
+    }
+
+    // Setter untuk nomor HP
+    public function setNoHp($no_hp) {
+        $this->no_hp = $no_hp;
+    }
+}
